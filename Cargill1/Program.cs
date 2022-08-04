42933afbd6577ae1cfbd738af4f5cbb0335136f8
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2022-08-04 17:27:31 +0200

    Own solution

--- Cargill1/Program.cs
+++ Cargill1/Program.cs
@@ -1,150 +1,52 @@
-﻿Computer mn = new Computer(CardType.Network, MonitorType.Mono);
-Computer mc = new Computer(CardType.CDRom, MonitorType.Mono);
-Computer mt = new Computer(CardType.Tape, MonitorType.Mono);
-Computer cn = new Computer(CardType.Network, MonitorType.Color);
-Computer cc = new Computer(CardType.CDRom, MonitorType.Color);
-Computer ct = new Computer(CardType.Tape, MonitorType.Color);
+﻿// mooier dan herhaalde code zijn loops
 
-mn.Print();
-mc.Print();
-mt.Print();
-cn.Print();
-cc.Print();
-ct.Print();
-
-internal enum CardType { CDRom, Tape, Network };
-
-internal enum MonitorType { Mono, Color };
-
-internal abstract class Card
+List<Monitor> monitors = new()
 {
-    public abstract int Price();
+    new() { Price = 500, Name = "Mono" },
+    new() { Price = 1500, Name = "Color" }
+};
 
-    public abstract string Name();
+List<Card> cards = new()
+{
+    new() { Price = 600, Name = "Network" },
+    new() { Price = 1500, Name = "CDRom", Rebate = 135 },
+    new() { Price = 1000, Name = "Tape" }
+};
 
-    public virtual int Rebate()
+foreach (Monitor monitor in monitors)
+{
+    foreach (Card card in cards)
     {
-        return 45;
+        Computer computer = new(card, monitor);
+        computer.Print();
     }
 }
 
-internal class Network : Card
+// Monitors en cards hebben allebei een name en price. Dus laat ik ze erven van een
+// component die WEL abstract is, want je wil geen generieke componenten kunnen maken.
+internal abstract class Component
 {
-    public override int Price()
-    {
-        return 600;
-    }
+    public int Price { get; init; }
 
-    public override string Name()
-    {
-        return "Network";
-    }
+    public string? Name { get; init; }
 }
 
-internal class CDRom : Card
+// Een card heeft een rebate. Ik zou rebate in Component kunnen doen,
+// maar daar is de logica in Computer niet op berekend.
+internal class Card : Component
 {
-    public override int Price()
-    {
-        return 1500;
-    }
-
-    public override string Name()
-    {
-        return "CDRom";
-    }
-
-    public override int Rebate()
-    {
-        return 135;
-    }
+    public int Rebate { get; init; } = 45;
 }
 
-internal class Tape : Card
+class Monitor : Component { }
+
+internal record Computer(Card Card, Monitor Monitor)
 {
-    public override int Price()
-    {
-        return 1000;
-    }
+    private int NetPrice() =>
+        Monitor.Price + Card.Price - Card.Rebate;
 
-    public override string Name()
-    {
-        return "Tape";
-    }
-}
-
-internal abstract class Monitor
-{
-    public abstract int Price();
-
-    public abstract string Name();
-}
-
-internal class Color : Monitor
-{
-    public override int Price()
-    {
-        return 1500;
-    }
-
-    public override string Name()
-    {
-        return "Color";
-    }
-}
-
-internal class Monochrome : Monitor
-{
-    public override int Price()
-    {
-        return 500;
-    }
-
-    public override string Name()
-    {
-        return "Mono";
-    }
-}
-
-internal class Computer
-{
-    private Card _card;
-    private Monitor _monitor;
-
-    public Computer(CardType cardType, MonitorType monitorType)
-    {
-        switch (cardType)
-        {
-            case CardType.Network:
-                _card = new Network();
-                break;
-            case CardType.CDRom:
-                _card = new CDRom();
-                break;
-            case CardType.Tape:
-                _card = new Tape();
-                break;
-        }
-
-        switch (monitorType)
-        {
-            case MonitorType.Mono:
-                _monitor = new Monochrome();
-                break;
-            case MonitorType.Color:
-                _monitor = new Color();
-                break;
-        }
-    }
-
-    private int NetPrice()
-    {
-        return _monitor.Price() + _card.Price() - _card.Rebate();
-    }
-
-    public void Print()
-    {
-        Console.WriteLine("{0} {1}, net price = {2}", _card.Name(), _monitor.Name(), NetPrice());
-    }
+    public void Print() =>
+        Console.WriteLine($"{Card.Name} {Monitor.Name}, net price = {NetPrice()}");
 }
 
 
